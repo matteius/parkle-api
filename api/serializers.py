22b7f0359c96daa6fd4f364c8b153f64bfe546a9
Matt Davis <matt.davis@cashstar.com>
2014-04-09 17:08:19 -0400

    Check in start of api app with some serializer definitions

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+
+class RequestPlayerSerializer(serializers.Serializer):
+    username_requested = serializers.CharField()
+    email_address = serializers.EmailField()
+
+
+class InitiateGameSerializer(serializers.Serializer):
+    player_api_key = serializers.CharField()
+    player_secret_key = serializers.CharField()
+    computer_bot_username = serializers.CharField(required=False)
+
+
+class GameStateSerializer(serializers.Serializer):
+    game_uuid = serializers.CharField()
+    player_api_key = serializers.CharField(required=False)
